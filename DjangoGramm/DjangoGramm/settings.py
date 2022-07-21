407d2867c6708e31aa100156b8df075165639cfe
Danil Tumanov <dania.tumanov@gmail.com>
2022-07-21 14:35:55 +0300

    change database to postgres

--- DjangoGramm/DjangoGramm/settings.py
+++ DjangoGramm/DjangoGramm/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'DjangoGramm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DjangoGramm',
+        'USER': 'postgres',
+        'PASSWORD': '1111',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
