5f9fa629abf6b54b1e0f07885cc2b19362816e4c
Danil Tumanov <dania.tumanov@gmail.com>
2022-07-24 15:54:41 +0300

    update class User

--- DjangoGramm/main/models.py
+++ DjangoGramm/main/models.py
@@ -7,7 +7,7 @@ class User(models.Model):
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     bio = models.CharField(max_length=250)
-    avatar = models.CharField(max_length=120)
+    avatar_src = models.CharField(max_length=120)
 
     def create_unique_link(self):
         return f'django-gram/activate/{User.pk}'
@@ -30,10 +30,10 @@ class User(models.Model):
         self.first_name = first_name
         self.last_name = last_name
         self.bio = bio
-        self.avatar = avatar
+        self.avatar_src = avatar
 
     def get_user(self):
-        return self.email, self.first_name, self.last_name, self.bio, self.avatar
+        return self.email, self.first_name, self.last_name, self.bio, self.avatar_src
 
 
 class FollowerFollowing(models.Model):
