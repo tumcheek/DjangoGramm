d33b1831eca423e82a40f91709c8f949105bee84
Danil Tumanov <dania.tumanov@gmail.com>
2022-07-25 14:52:06 +0300

    update classes UserModel and PostModel

--- DjangoGramm/main/models.py
+++ DjangoGramm/main/models.py
@@ -1,78 +1,47 @@
 from django.db import models
-from django.core.mail import send_mail
 
 
-class User(models.Model):
+class UserModel(models.Model):
     email = models.EmailField()
     first_name = models.CharField(max_length=120)
     last_name = models.CharField(max_length=120)
     bio = models.CharField(max_length=250)
     avatar_src = models.CharField(max_length=120)
 
-    def create_unique_link(self):
-        return f'django-gram/activate/{User.pk}'
 
-    def send_unique_link(self):
-
-        send_mail(
-            subject='Activation',
-            message=self.create_unique_link(),
-            from_email='from@example.com',
-            recipient_list=[self.email],
-            fail_silently=False,
-        )
-
-    def create_user(self, email):
-        self.email = email
-        self.send_unique_link()
-
-    def set_user(self, first_name, last_name, bio, avatar):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.bio = bio
-        self.avatar_src = avatar
-
-    def get_user(self):
-        return self.email, self.first_name, self.last_name, self.bio, self.avatar_src
+class FollowerFollowingModel(models.Model):
+    follower_id = models.ManyToManyField(UserModel)
+    following_id = models.ManyToManyField(UserModel)
 
 
-class FollowerFollowing(models.Model):
-    follower_id = User()
-    following_id = User()
-
-
-class MediaType(models.Model):
+class MediaTypeModel(models.Model):
     name = models.CharField(max_length=10)
 
 
-class Media(models.Model):
-    media_type_id = models.ForeignKey(MediaType, on_delete=models.CASCADE)
+class MediaModel(models.Model):
+    media_type_id = models.ForeignKey(MediaTypeModel, on_delete=models.CASCADE)
     media_src = models.CharField(max_length=100)
 
 
-class Post(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.ManyToManyField(Media)
+class PostModel(models.Model):
+    user_id = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    content = models.ManyToManyField(MediaModel)
     create_at = models.DateTimeField('date published')
 
-    def create_post(self, user_id, content):
-        self.user_id = user_id
-        self.content = content
+
+class BaseModel(models.Model):
+    post_id = models.ManyToManyField(PostModel)
+    user_id = models.ManyToManyField(UserModel)
 
 
-class Base(models.Model):
-    post_id = models.ManyToManyField(Post)
-    user_id = models.ManyToManyField(User)
-
-
-class Like(Base):
+class LikeModel(BaseModel):
     likes = models.IntegerField(default=0)
 
 
-class Tag(Base):
+class TagModel(BaseModel):
     tags = models.CharField(max_length=50)
 
 
-class Bookmarks(Base):
+class BookmarksModel(BaseModel):
     is_bookmark = models.BooleanField(default=False)
 
