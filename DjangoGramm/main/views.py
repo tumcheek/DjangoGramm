d6efa2b64de383dc622dfd2c1d51a584a7da500c
Danil Tumanov <dania.tumanov@gmail.com>
2022-08-20 20:16:26 +0300

    add ProfileSettingView

--- DjangoGramm/main/views.py
+++ DjangoGramm/main/views.py
@@ -4,13 +4,36 @@ from django.utils.http import urlsafe_base64_decode
 from django.views import generic, View
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, get_user_model
+from django.contrib.auth import login, authenticate, get_user_model, logout
 from .forms import SignupForm
 from django.contrib.auth.tokens import default_token_generator as \
     token_generator
 from .utils import send_email_for_verify
+from django.core.files.storage import FileSystemStorage
+from pathlib import Path
 
 User = get_user_model()
+
+
+class ProfileSettingView(View):
+    template_name = 'main/registration/profile_settings.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            return render(request, self.template_name)
+        else:
+            return HttpResponse('You must login!')
+
+    def post(self, request):
+        form = request.POST
+        uploaded_avatar = request.FILES.get('avatar')
+        user = request.user
+        user.avatar_src = uploaded_avatar
+        user.first_name = form['first_name']
+        user.last_name = form['last_name']
+        user.bio = form['bio']
+        user.save()
+        return redirect('main:profile')
 
 
 class EmailVerifyView(View):
@@ -20,8 +43,7 @@ class EmailVerifyView(View):
             user.is_verify = True
             user.save()
             login(request, user)
-            return redirect('main:register')
-
+            return redirect('main:profile_setting')
 
     @staticmethod
     def get_user(uidb64):
