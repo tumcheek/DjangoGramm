3c12806ba29f8b2f4e283baf546cf2aa25665bf8
Danil Tumanov <dania.tumanov@gmail.com>
2022-08-21 11:57:06 +0300

    add profile and profile setting path

--- DjangoGramm/main/urls.py
+++ DjangoGramm/main/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from django.views.generic import TemplateView
 
@@ -9,4 +11,7 @@ urlpatterns = [
     path('confirm_email', TemplateView.as_view(template_name='registration/confirm_email.html'), name='confirm_email'),
     path('verify_email/<uidb64>/<token>/', views.EmailVerifyView.as_view(), name='verify_email'),
     path('registration/', views.RegistrationView.as_view(), name='register'),
+    path('profile/settings/', views.ProfileSettingView.as_view(), name='profile_setting'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
+
