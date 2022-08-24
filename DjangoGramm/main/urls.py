f694faa37172615ee297b29ac6db5be5c443c73f
Danil Tumanov <dania.tumanov@gmail.com>
2022-08-24 13:29:55 +0300

    modify confirm_email path

--- DjangoGramm/main/urls.py
+++ DjangoGramm/main/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-    path('confirm_email', TemplateView.as_view(template_name='registration/confirm_email.html'), name='confirm_email'),
+    path('confirm_email', TemplateView.as_view(template_name='main/registration/confirm_email.html'), name='confirm_email'),
     path('verify_email/<uidb64>/<token>/', views.EmailVerifyView.as_view(), name='verify_email'),
     path('registration/', views.RegistrationView.as_view(), name='register'),
     path('profile/settings/', views.ProfileSettingView.as_view(), name='profile_setting'),
