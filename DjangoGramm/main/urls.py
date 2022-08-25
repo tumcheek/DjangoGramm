bcb7b3afad948accb295c4b230d8ce9b2fe11411
Danil Tumanov <dania.tumanov@gmail.com>
2022-08-25 17:38:43 +0300

    add pathes logout, login and confirm_error

--- DjangoGramm/main/urls.py
+++ DjangoGramm/main/urls.py
@@ -1,15 +1,21 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 app_name = 'main'
 
 urlpatterns = [
-    path('confirm_email', TemplateView.as_view(template_name='main/registration/confirm_email.html'), name='confirm_email'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('confirm_email/', TemplateView.as_view(template_name='main/registration/confirm_email.html'),
+         name='confirm_email'),
     path('verify_email/<uidb64>/<token>/', views.EmailVerifyView.as_view(), name='verify_email'),
+    path('confirm_email/error/', TemplateView.as_view(template_name='main/registration/confirm_error.html'),
+         name='confirm_error'),
     path('registration/', views.RegistrationView.as_view(), name='register'),
     path('profile/settings/', views.ProfileSettingView.as_view(), name='profile_setting'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
