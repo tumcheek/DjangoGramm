3af0e38f131a5ffc966c3de5d3ebfd3b45ff612a
Danil Tumanov <dania.tumanov@gmail.com>
2022-08-31 12:20:16 +0300

    add function get_post_info

--- DjangoGramm/main/utils.py
+++ DjangoGramm/main/utils.py
@@ -30,7 +30,7 @@ def send_email_for_verify(request, user):
     email.send()
 
 
-def get_posts_list(posts_query, request):
+def get_posts_list(posts_query, user):
     posts = []
 
     for post in posts_query:
@@ -47,10 +47,34 @@ def get_posts_list(posts_query, request):
         likes = post.likemodel_set.all().count()
         post_info.append(likes)
         post_info.append(post.pk)
-        is_liked = True if post.likemodel_set.filter(user_id=request.user.pk) else False
+        is_liked = True if post.likemodel_set.filter(user_id=user.pk) else False
         post_info.append(is_liked)
-        is_bookmark = False if post.bookmarksmodel_set.filter(user_id=request.user.pk) else True
+        is_bookmark = False if post.bookmarksmodel_set.filter(user_id=user.pk) else True
         post_info.append(is_bookmark)
         posts.append(post_info)
 
     return posts
+
+
+def get_post_info(post, user, post_creator):
+    all_post_media = []
+    all_post_tags = []
+    for media in post.medias.all():
+        all_post_media.append(media.media_src)
+    for tag in TagModel.objects.filter(post=post.pk):
+        all_post_tags.append(tag.name)
+    post_info = {
+        'post_creator': post_creator.username,
+        'post_creator_avatar': post_creator.avatar_src,
+        'post_content': post.content,
+        'created_at': post.created_at,
+        'post_pk': post.pk,
+        'media': all_post_media,
+        'tags': all_post_tags,
+        'likes': post.likemodel_set.all().count(),
+        'is_liked': True if post.likemodel_set.filter(user_id=user.pk) else False,
+        'is_bookmark': False if post.bookmarksmodel_set.filter(user_id=user.pk) else True
+
+    }
+
+    return post_info
