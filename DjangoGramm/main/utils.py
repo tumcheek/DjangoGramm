b3232aec9148678f12161861e0bc0df269c29342
Danil Tumanov <dania.tumanov@gmail.com>
2022-08-31 17:00:13 +0300

    delete get_posts_list

--- DjangoGramm/main/utils.py
+++ DjangoGramm/main/utils.py
@@ -30,32 +30,6 @@ def send_email_for_verify(request, user):
     email.send()
 
 
-def get_posts_list(posts_query, user):
-    posts = []
-
-    for post in posts_query:
-        post_info = []
-        all_post_media = []
-        all_post_tags = []
-        post_info.append(post.content)
-        for media in post.medias.all():
-            all_post_media.append(media.media_src)
-        post_info.append(all_post_media)
-        for tag in TagModel.objects.filter(post=post.pk):
-            all_post_tags.append(tag.name)
-        post_info.append(all_post_tags)
-        likes = post.likemodel_set.all().count()
-        post_info.append(likes)
-        post_info.append(post.pk)
-        is_liked = True if post.likemodel_set.filter(user_id=user.pk) else False
-        post_info.append(is_liked)
-        is_bookmark = False if post.bookmarksmodel_set.filter(user_id=user.pk) else True
-        post_info.append(is_bookmark)
-        posts.append(post_info)
-
-    return posts
-
-
 def get_user_posts_info(posts_query, user, post_creator):
     posts = []
     for post in posts_query:
