b890ad0012403f90a3f00e1b0a4547d924262e73
Danil Tumanov <dania.tumanov@gmail.com>
2022-09-07 13:19:10 +0300

    change the loops to functions

--- DjangoGramm/main/management/commands/setup_test_data.py
+++ DjangoGramm/main/management/commands/setup_test_data.py
@@ -3,15 +3,15 @@ import random
 from django.db import transaction
 from django.core.management.base import BaseCommand
 
-from main.models import UserModel, MediaTypeModel, FollowerFollowingModel, MediaModel, PostModel,\
-    LikeModel, TagModel, BookmarksModel
-from main.factories import UserFactory, FollowerFollowingFactory, MediaTypeFactory, MediaFactory, \
-    PostFactory, LikeFactory, TagFactory, BookmarksFactory
+from .functions_for_fake_data_create import *
 
-NUM_USERS = 50
+NUM_USERS = 200
 NUM_MEDIA = 10
 NUM_POSTS = 250
-NUM_FOLLOWER_FOLLOWING = 50
+NUM_FOLLOWER_FOLLOWING = 1000
+NUM_LIKES = 100
+NUM_BOOKMARKS = 100
+NUM_TAGS = 100
 MEDIA_TYPE = ['png', 'jpg', 'jpeg']
 
 
@@ -21,49 +21,17 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
         self.stdout.write("Deleting old data...")
-        UserModel.objects.all().delete()
-        FollowerFollowingModel.objects.all().delete()
-        MediaTypeModel.objects.all().delete()
-        MediaModel.objects.all().delete()
-        PostModel.objects.all().delete()
-        LikeModel.objects.all().delete()
-        TagModel.objects.all().delete()
-        BookmarksModel.objects.all().delete()
 
-        people = []
-        medias = []
-        posts = []
-        media_types = []
+        delete_all_data()
 
-        for _ in range(NUM_USERS):
-            person = UserFactory(is_verify=True)
-            people.append(person)
+        people = create_fake_users(NUM_USERS)
+        media_types = create_fake_media_types(MEDIA_TYPE)
+        medias = create_fake_medias(NUM_MEDIA, media_types)
+        posts = create_fake_posts(NUM_POSTS, people, medias)
 
-        for _ in range(NUM_FOLLOWER_FOLLOWING):
-            FollowerFollowingFactory(followers=random.choice(people), following=random.choice(people))
-
-        for _type in MEDIA_TYPE:
-            media_type = MediaTypeFactory(name=_type)
-            media_types.append(media_type)
-
-        for _ in range(NUM_MEDIA):
-            media = MediaFactory(media_type=random.choice(media_types))
-            medias.append(media)
-
-        for _ in range(NUM_POSTS):
-            post = PostFactory(user=random.choice(people))
-            current_media = random.choices(medias, k=8)
-            post.medias.add(*current_media)
-            posts.append(post)
-
-        for _ in range(50):
-            users = random.choices(people, k=8)
-            current_post = random.choices(posts, k=8)
-            LikeFactory(user=random.choice(people), post=random.choice(posts))
-            tag = TagFactory()
-            tag.post.add(*current_post)
-            tag.user.add(*users)
-            BookmarksFactory(user=random.choice(people), post=random.choice(posts))
-
+        create_fake_follower_following(NUM_FOLLOWER_FOLLOWING, people)
+        create_fake_likes(NUM_LIKES, people, posts)
+        create_fake_bookmarks(NUM_BOOKMARKS, people, posts)
+        create_fake_tags(NUM_TAGS, posts, people)
 
 
