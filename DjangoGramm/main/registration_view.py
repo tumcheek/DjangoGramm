d75263d51e1bf27efb6da7789fa75031ee91ab81
Danil Tumanov <dania.tumanov@gmail.com>
2022-09-14 18:26:27 +0300

    modify RegistrationView class

--- DjangoGramm/main/registration_view.py
+++ DjangoGramm/main/registration_view.py
@@ -1,6 +1,8 @@
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.template.loader import render_to_string
+from django.urls import reverse
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from django.contrib.auth.tokens import default_token_generator as \
@@ -8,8 +10,10 @@ from django.contrib.auth.tokens import default_token_generator as \
 
 from django.views import View
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from .forms import SignupForm
+
+User = get_user_model()
 
 
 def send_email_for_verify(request, user):
@@ -37,22 +41,31 @@ def send_email_for_verify(request, user):
 class RegistrationView(View):
     template_name = 'main/registration/register.html'
 
-    def get(self, request):
-        context = {
-            'form': SignupForm()
-        }
+    def get(self, request, complete=None):
+        if complete == 'complete':
+            context = {
+                'is_complete': True
+            }
+        else:
+            context = {
+                'form': SignupForm(),
+                'is_complete': False
+            }
         return render(request, self.template_name, context)
 
     def post(self, request):
         form = SignupForm(request.POST)
 
         if form.is_valid():
-            form.save()
-            email = form.cleaned_data.get('email')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(email=email, password=password)
-            send_email_for_verify(request, user)
-            return redirect('main:confirm_email')
+            try:
+                form.save()
+                email = form.cleaned_data.get('email')
+                password = form.cleaned_data.get('password1')
+                user = authenticate(email=email, password=password)
+                send_email_for_verify(request, user)
+                return redirect(reverse('main:register', args=['complete']))
+            except User.DoesNotExist:
+                return HttpResponse(status=500)
         context = {
             'form': form
         }
